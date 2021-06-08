14b1cf5527712f1211381c052f6abcc846e1a1b6
Vladan Grgur <grgurvladan@gmail.com>
2021-06-09 01:00:24 +0200

    Initial commit

---
+++ Hospital.Center/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Hospital.Common.Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Hospital.Center.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LoginModel user)
+        {
+            if (user == null)
+                return BadRequest("Invalid client request");
+
+
+            if (user.UserName == "john" && user.Password == "pass123")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hospital@hospital@hospital"));
+                var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:5001",
+                    audience: "http://localhost:5001",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: credentials
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ Hospital.Center/Extensions/ServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Center.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("EnableCORS", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+        }
+
+
+        public static void ConfigureAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "http://localhost:5001",
+                    ValidAudience = "http://localhost:5001",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("@hospital@hospital@hospital"))
+                };
+            });
+        }
+
+
+    }
+}
